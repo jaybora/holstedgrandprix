5c2982330b52e019e26671a29783ec45c04cd593
jer <jer@brunata.dk>
2016-05-02 08:19:55 +0200

    Added day program

--- server/src/client/html/js/app.js
+++ server/src/client/html/js/app.js
@@ -1,11 +1,12 @@
 angular.module('grandprix', ['ngRoute', 'ngAnimate', 'angular-google-gapi', 'ui.bootstrap'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/frontpage', {templateUrl: 'partials/frontpage.html', controller: 'FrontpageCtrl'});
+        $routeProvider.when('/dayprogram', {templateUrl: 'partials/dayprogram.html'});
         $routeProvider.when('/program', {templateUrl: 'partials/program.html', controller: 'ProgramCtrl'});
         $routeProvider.when('/programtest', {templateUrl: 'partials/programtest.html', controller: 'ProgramCtrl'});
         $routeProvider.when('/rules', {templateUrl: 'partials/rules.html'});
         $routeProvider.when('/2015', {templateUrl: 'partials/2015.html'});
-        $routeProvider.otherwise({redirectTo: 'frontpage'});
+        $routeProvider.otherwise({redirectTo: 'dayprogram'});
     }])
     .run(['GApi', 'GAuth', function(GApi, GAuth) {
         var BASE = '/_ah/api';
@@ -15,6 +16,46 @@ angular.module('grandprix', ['ngRoute', 'ngAnimate', 'angular-google-gapi', 'ui.
             console.log('an error occured during loading api: ' + resp.api + ', resp.version: ' + version);
         });
     }])
+    .directive('countdown', [
+        'Util',
+        '$interval',
+        function (Util, $interval) {
+            return {
+                restrict: 'A',
+                scope: { date: '@' },
+                link: function (scope, element) {
+                    var future;
+                    future = new Date(scope.date);
+                    $interval(function () {
+                        var diff;
+                        diff = Math.floor((future.getTime() - new Date().getTime()) / 1000);
+                        return element.text(Util.dhms(diff));
+                    }, 1000);
+                }
+            };
+        }
+    ])
+    .factory('Util', [function () {
+            return {
+                dhms: function (t) {
+                    var days, hours, minutes, seconds;
+                    days = Math.floor(t / 86400);
+                    t -= days * 86400;
+                    hours = Math.floor(t / 3600) % 24;
+                    t -= hours * 3600;
+                    minutes = Math.floor(t / 60) % 60;
+                    t -= minutes * 60;
+                    seconds = t % 60;
+                    return [
+                        days + ' dage',
+                        hours + ' timer',
+                        minutes + ' minutter',
+                        seconds + ' sekunder'
+                    ].join(' ');
+                }
+            };
+        }
+    ])
     .filter('yesNo', function() {
         return function(input) {
             return input ? 'Ja' : 'Nej';
